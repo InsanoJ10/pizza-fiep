f56a86d0bea7bc6bcb768809b11cf6ab485b3a04
Júlio <tdsjsa2010@gmail.com>
2026-08-20 09:42:17 -0300

    Add files via upload

---
+++ pizza-fiep/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Diferenciais from "./components/Diferenciais";
+function App(){
+  return(
+    <>
+      <Header />
+      <Hero />
+      <Diferenciais/>
+    </>
+  )
+}
+
+export default App;
