4727ce9bebf8d5275309ed10f6fd90361fdcb4b0
Júlio <tdsjsa2010@gmail.com>
2026-08-21 09:40:24 -0300

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Diferenciais from "./components/Diferenciais";
 import Cardapio from "./components/Cardapio";
+import Sobre from "./components/Sobre";
 function App(){
   return(
     <>
@@ -9,6 +10,7 @@ function App(){
       <Hero />
       <Diferenciais/>
       <Cardapio/>
+      <Sobre/>
     </>
   )
 }
